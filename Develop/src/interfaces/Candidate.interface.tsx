9796527418def1eb3eacb7a04ee41f60c642f543
Brijae <brijaewilliams5@gmail.com>
2024-12-18 16:09:28 -0500

    Added and completed nav bar and compontents

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,20 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface CandidateInterface {
+    id: number;
+    login: string;
+    avatar_url: string;
+    html_url: string;
+    name?: string;
+    company?: string;
+    blog?: string;
+    location?: string;
+    email?: string;
+    bio?: string;
+    twitter_username?: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+    created_at: string;
+    updated_at: string;
+  }
+  export default CandidateInterface;
